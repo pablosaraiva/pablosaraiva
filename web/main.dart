14454551be2b823c1580420307871d12b10c4e89
Pablo Saraiva <pablosaraiva@gmail.com>
2015-05-25 09:49:19 -0300

    Moved routing to a better place

--- web/main.dart
+++ web/main.dart
@@ -1,39 +1,15 @@
 import 'package:polymer/polymer.dart';
 import 'dart:html';
-import 'package:route_hierarchical/client.dart';
 
 
 main() => initPolymer();
 
-void showBlog(RouteEvent e) {
-  print("showBlog");
-  if (e.parameters.isNotEmpty) {
-    var postId = e.parameters['postId'];
-    print("Post id: " + postId);
-  } else {
-    print("Last post");
-  }
-}
 
-void showLeitura(RouteEvent e) {
-  print("showLeitura");
-}
-
-void showSobre(RouteEvent e) {
-  print("showSobre");
-}
 
 
 @initMethod
 realMain() {
-  var router = new Router(useFragment: true);
-  
-  router.root
-      ..addRoute(name: 'blog', defaultRoute: true, path: 'blog/:postId', enter: showBlog)
-      ..addRoute(name: 'leitura', path: 'leitura', enter: showLeitura)
-      ..addRoute(name: 'sobre', path: 'sobre', enter: showSobre);
-  router.listen();
-  
+
   Polymer.onReady.then((_) {
   });    
   
--- web/pablosaraiva/app/pablosaraiva-app.dart
+++ web/pablosaraiva/app/pablosaraiva-app.dart
@@ -1,5 +1,6 @@
 import 'package:polymer/polymer.dart';
 import 'package:core_elements/core_drawer_panel.dart';
+import 'package:route_hierarchical/client.dart';
 
 
 @CustomTag('pablosaraiva-app')
@@ -8,6 +9,7 @@ class PablosaraivaApp extends PolymerElement {
   String page = "blog";
   
   PablosaraivaApp.created() : super.created();
+  var router = new Router(useFragment: true);
 
   @override
   void ready() {
@@ -15,6 +17,32 @@ class PablosaraivaApp extends PolymerElement {
     CoreDrawerPanel panel = shadowRoot.querySelector('#panel');
     panel.responsiveWidth = "800pt";
     panel.onClick.listen((e) => panel.togglePanel());
+    
+    
+    router.root
+        ..addRoute(name: 'blog', defaultRoute: true, path: 'blog/:postId', enter: showBlog)
+        ..addRoute(name: 'leitura', path: 'leitura', enter: showLeitura)
+        ..addRoute(name: 'sobre', path: 'sobre', enter: showSobre);
+    router.listen();
+        
+  }
+  
+  void showBlog(RouteEvent e) {
+    print("showBlog");
+    if (e.parameters.isNotEmpty) {
+      var postId = e.parameters['postId'];
+      print("Post id: " + postId);
+    } else {
+      print("Last post");
+    }
+  }
+
+  void showLeitura(RouteEvent e) {
+    print("showLeitura");
+  }
+
+  void showSobre(RouteEvent e) {
+    print("showSobre");
   }
 
 }
