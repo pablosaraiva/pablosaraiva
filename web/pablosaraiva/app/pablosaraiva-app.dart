f687a785f73454d5c9beeaeb688d25397158418d
Pablo Saraiva <pablosaraiva@gmail.com>
2015-05-25 11:15:52 -0300

    Scroll to top after page change

--- web/pablosaraiva/app/pablosaraiva-app.dart
+++ web/pablosaraiva/app/pablosaraiva-app.dart
@@ -1,6 +1,9 @@
 import 'package:polymer/polymer.dart';
 import 'package:core_elements/core_drawer_panel.dart';
+import 'package:core_elements/core_header_panel.dart';
+
 import 'package:route_hierarchical/client.dart';
+import 'dart:html';
 
 
 @CustomTag('pablosaraiva-app')
@@ -11,10 +14,12 @@ class PablosaraivaApp extends PolymerElement {
   PablosaraivaApp.created() : super.created();
   var router = new Router(useFragment: true);
 
+  CoreDrawerPanel panel;
+  
   @override
   void ready() {
     super.ready();
-    CoreDrawerPanel panel = shadowRoot.querySelector('#panel');
+    panel = shadowRoot.querySelector('#panel');
     panel.responsiveWidth = "800pt";
     panel.onClick.listen((e) => panel.togglePanel());
     
@@ -27,6 +32,7 @@ class PablosaraivaApp extends PolymerElement {
   }
   
   void showBlog(RouteEvent e) {
+    scrollToPageTop();
     page = "blog";
     if (e.parameters.isNotEmpty) {
 //      var postId = e.parameters['postId'];
@@ -35,11 +41,17 @@ class PablosaraivaApp extends PolymerElement {
   }
 
   void showLeitura(RouteEvent e) {
+    scrollToPageTop();
     page = "leitura";
   }
 
   void showSobre(RouteEvent e) {
+    scrollToPageTop();
     page = "sobre";
   }
-
+  
+  void scrollToPageTop() {
+    CoreHeaderPanel mainHeaderPanel = shadowRoot.querySelector('#mainHeaderPanel');
+    mainHeaderPanel.scroller.scrollTop = 0;
+  }
 }
